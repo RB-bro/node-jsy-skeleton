548094df829b5b9a4948233e6e73c60730d49251
Brandon Brown <proby@scriberule.com>
2017-03-22 23:42:13 -0500

    Updated dependency for jsy

--- tests/all.js
+++ tests/all.js
@@ -2,7 +2,7 @@
 
 import runTests from 'tap-lite-tester/runner'
 
-if (module === require.main)
+if module === require.main ::
   runTests @
     process.argv.slice @ 2
   .catch @ err => console.error @ err
